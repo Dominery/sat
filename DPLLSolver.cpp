9b52e754dcab3624f8e06908367a983965d57736
苏宇 <54257234+Dominery@users.noreply.github.com>
2021-03-11 10:13:43 +0800

    basic dpll solver

--- DPLLSolver.cpp
+++ DPLLSolver.cpp
@@ -1,14 +1,17 @@
+#include<vector>
+#include<algorithm>
 #include "DPLLSolver.h"
+using namespace std;
 
 Status DPLLSolver::process(){
     Status status = preprocess();
     if(status != UNKNOWN)return status;
     while (1)
     {
-        decide_next_branch();
+        int literal = decide_next_branch();
         while (true)
         {
-            status = deduce();
+            status = deduce(literal);
             if(status == CONFLICT){
                 int level = analyze_conflict();
                 if(level==0)return UNSATISFIABLE;
@@ -20,4 +23,82 @@ Status DPLLSolver::process(){
         
     }
     
+}
+
+Status DPLLSolver::single_clause(Formula& formula){
+    if(formula.clauses.size()==0)return SATISFIABLE;
+    // single_found will be true until the formula doesn't have single clauses
+    bool single_found = true;
+    while (single_found)
+    {
+        single_found = false;
+        auto clause_num = formula.clauses.size();
+        for(int i=0;i<clause_num;i++){
+            if(formula.clauses[i].size()==1){
+                single_found = true;
+                int literal = formula.clauses[i][0];
+                formula.literals[literal/2] = literal%2;
+                formula.literal_frequency[literal/2]=-1; // mark the literal frequency
+                Status status = transform_clauses(formula,literal/2);
+                if (status==UNKNOWN)break;
+                else return status;
+            }
+        }
+    }
+    return UNKNOWN;
+    
+}
+
+Status DPLLSolver::transform_clauses(Formula &formula,int literal){
+    int value_of_literal = formula.literals[literal]; // 0 true;1 false
+    for(int i=0;i<formula.clauses.size();i++){
+        for(int j=0;j<formula.clauses[i].size();j++){
+            if(2*literal+value_of_literal==formula.clauses[i][j]){
+                formula.clauses.erase(formula.clauses.begin()+i);
+                i--;
+                if(formula.clauses.size()==0)return SATISFIABLE;
+                break;
+            }else{
+                formula.clauses[i].erase(formula.clauses[i].begin()+j);
+                j--;
+                if(formula.clauses[i].size()==0)return UNSATISFIABLE;
+            }
+        }
+    }
+    return UNKNOWN;
+}
+
+Status DPLLSolver::preprocess(){
+    Status status = single_clause(current_node);
+    fstack.push(current_node);
+    return status;
+}
+
+int DPLLSolver::decide_next_branch(){
+    //find the literal with maximum frequency in current formula
+    int i = distance(current_node.literal_frequency.begin(),
+    max_element(current_node.literal_frequency.begin(),current_node.literal_frequency.end()));
+    current_node.literals[i] = 1;
+    fstack.push(current_node);
+    current_node.literals[i]=0;
+    return i;
+}
+
+Status DPLLSolver::deduce(int literal){
+    Status status = transform_clauses(current_node,literal);
+    if (status==UNSATISFIABLE)return CONFLICT;
+    status = single_clause(current_node);
+    if(status == UNSATISFIABLE)return CONFLICT;
+    return status;
+}
+
+int DPLLSolver::analyze_conflict(){
+    return 1;
+}
+
+void DPLLSolver::backtrack(int level){
+    for(int i=0;i<level;i++){
+        current_node = fstack.top();
+        fstack.pop();
+    }
 }
--- DPLLSolver.h
+++ DPLLSolver.h
@@ -1,3 +1,5 @@
+#include <stack>
+#include "formula.h"
 #ifndef DPLL_PROCESSOR_H
 #define DPLL_PROCESSOR_H
 
@@ -6,18 +8,31 @@ enum Status{UNKNOWN=2,SATISFIABLE=1,UNSATISFIABLE=0,CONFLICT};
 class DPLLSolver
 {
 private:
+    // stack contain all state of formula 
+    // which can help for backtrack
+    std::stack<Formula> fstack; 
+    // current_node is the current formula 
+    Formula current_node;
+    // preprocess the formula and then push it to fstack
     Status preprocess();
-    void decide_next_branch();
-    Status deduce();
+    // change the current formula
+    int decide_next_branch();
+    // decrease the clauses in formula using rule
+    Status deduce(int);
+    // return where the fstack don't have conflicts
     int analyze_conflict();
+    // pop the formula of fstack and change the current formula
     void backtrack(int level);
+    // if clause only has a literal,make the literal true and transform clauses
+    Status single_clause(Formula&);
+    Status transform_clauses(Formula&,int literal);
 public:
-    DPLLSolver();
+    DPLLSolver(Formula&);
     ~DPLLSolver();
     Status process();
 };
 
-DPLLSolver::DPLLSolver()
+DPLLSolver::DPLLSolver(Formula& formula):current_node(formula)
 {
 }
 
